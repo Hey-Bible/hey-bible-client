f1d0a4415e7aa7acc5813e97b310a002ccb8c324
Bobby Galli <bobbyg603@gmail.com>
2025-07-06 20:56:42 -0400

    fix: top level array type parsing

--- typescript/README.md
+++ typescript/README.md
@@ -1,4 +1,4 @@
-## hey-bible-client@1.0.2
+## hey-bible-client@1.0.3
 
 This generator creates TypeScript/JavaScript client that utilizes [Fetch API](https://fetch.spec.whatwg.org/). The generated Node module can be used in the following environments:
 
@@ -36,7 +36,7 @@ navigate to the folder of your consuming project and run one of the following co
 _published:_
 
 ```
-npm install hey-bible-client@1.0.2 --save
+npm install hey-bible-client@1.0.3 --save
 ```
 
 _unPublished (not recommended):_
--- typescript/src/models/index.ts
+++ typescript/src/models/index.ts
@@ -1,18 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './ImagesGet200Response';
-export * from './ImagesGet200ResponseOneOf';
-export * from './ImagesGet200ResponseOneOf1';
-export * from './ImagesGet200ResponseOneOf1ImagesInner';
-export * from './ImagesGet200ResponseOneOfImages';
+export * from './ImagesGet200ResponseImagesInner';
 export * from './NotesGet200Response';
-export * from './NotesGet200ResponseOneOf';
-export * from './NotesGet200ResponseOneOf1';
-export * from './NotesGet200ResponseOneOf1NotesInner';
-export * from './NotesGet200ResponseOneOfNotes';
-export * from './NotesGet200ResponseOneOfNotesVerses';
+export * from './NotesGet200ResponseNotesInner';
 export * from './VersesGet200Response';
-export * from './VersesGet200ResponseOneOf';
-export * from './VersesGet200ResponseOneOf1';
-export * from './VersesGet200ResponseOneOf1VersesInner';
-export * from './VersesGet200ResponseOneOfVerses';
+export * from './VersesGet200ResponseVersesInner';
+export * from './VersesGet200ResponseVersesInnerImagesInner';
